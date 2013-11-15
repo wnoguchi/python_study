36f951d06fc078a7b22a99871c90fef36d3aaeb7
Wataru Noguchi <wnoguchi@unicast.ne.jp>
2013-11-15 20:37:03 +0900

    Initial commit.

---
+++ windows/hello.py
@@ -0,0 +1,12 @@
+#! C:/Python27/python.exe
+# -*- coding: utf-8 -*- 
+
+print u"helloモジュールのロード"
+
+def say_hello():
+  print u"hello, python world."
+
+if __name__ == "__main__":
+
+  print u"main関数内"
+  say_hello()
