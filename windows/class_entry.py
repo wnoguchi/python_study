609ba902022e7a25074827d4a72005ecdf785d03
Wataru Noguchi <wnoguchi@unicast.ne.jp>
2013-11-20 19:31:06 +0900

    Simplify.

--- windows/class_entry.py
+++ windows/class_entry.py
@@ -4,11 +4,12 @@
 # これはコメントです
 
 import test_class
+from test_class import test_class
 
 if __name__ == "__main__":
   classList = []
-  classList.append(test_class.test_class(1, u"テスト1"))
-  classList.append(test_class.test_class(2, u"テスト2"))
+  classList.append(test_class(1, u"テスト1"))
+  classList.append(test_class(2, u"テスト2"))
 
   for value in classList:
     print "===== class ====="
