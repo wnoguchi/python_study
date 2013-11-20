3b584b25fb2d8a3b3fc6737bc2be021e8122d742
Wataru Noguchi <wnoguchi@unicast.ne.jp>
2013-11-20 19:11:16 +0900

    commit.

--- windows/hello.py
+++ windows/hello.py
@@ -5,10 +5,20 @@
 
 print u"helloモジュールのロード"
 
+multiline = u"""
+aaa
+bbb
+ccc
+吾輩は猫である
+"""
+
 def say_hello():
   print u"hello, python world."
 
 if __name__ == "__main__":
 
-  print u"main関数内"
+  print u"main関数内" + u"ああああああああああ"
   say_hello()
+  print multiline
+
+  print str(100 + 20) + u"円"
